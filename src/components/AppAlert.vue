d42a5e6c625da66e1d2866b4cada87ee4c330233
jaei Kim <dobbykim0320@gmail.com>
2024-07-11 14:03:57 +0900

    feat: TransitionGroup for Alert Component

--- src/components/AppAlert.vue
+++ src/components/AppAlert.vue
@@ -1,32 +1,56 @@
 <template>
-  <Transition name="slide">
+  <!-- Transition 
+    <Transition name="slide">
     <div v-if="show" class="app-alert alert" :class="typeStyle" role="alert">
       {{ message }}
     </div>
-  </Transition>
+  </Transition> -->
+  <!-- Transition Group -->
+  <div class="app-alert">
+    <TransitionGroup name="slide">
+      <div
+        v-for="({ message, type }, index) in items"
+        :key="index"
+        class="alert"
+        :class="typeStyle(type)"
+        role="alert"
+      >
+        {{ message }}
+      </div>
+    </TransitionGroup>
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-
-const props = defineProps({
-  show: {
-    type: Boolean,
-    default: false,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    default: "error",
-    validator: (value) => ["success", "error"].includes(value),
-  },
+//Transition Group
+defineProps({
+  items: Array,
 });
-const typeStyle = computed(() =>
-  props.type === "error" ? "alert-danger" : "alert-success"
-);
+
+const typeStyle = (type) => {
+  // console.log("type: ", type);
+  return type === "error" ? "alert-danger" : "alert-success";
+};
+
+// Transition
+// const props = defineProps({
+//   show: {
+//     type: Boolean,
+//     default: false,
+//   },
+//   message: {
+//     type: String,
+//     required: true,
+//   },
+//   type: {
+//     type: String,
+//     default: "error",
+//     validator: (value) => ["success", "error"].includes(value),
+//   },
+// });
+// const typeStyle = computed(() =>
+//   props.type === "error" ? "alert-danger" : "alert-success"
+// );
 </script>
 
 <style scoped>
