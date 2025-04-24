f7804713f430eec6164bae055c976ba47d3da128
Jaei Kim <dobbykim0320@gmail.com>
2025-04-24 14:51:49 +0900

    fetch: nuxt pageComponent"

--- app.vue
+++ app.vue
@@ -1,11 +1,64 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <Welcom />
-    <Counter />
+    <div class="custom-box">
+      <button @click="toggle">Toggle</button>
+      <p>{{ checking }}</p>
+    </div>
+    <div class="custom-box" style="justify-content: center">
+      <!-- Toggle 버전 -->
+      <div class="custom-window">
+        <component :is="myComponent" />
+      </div>
+      <!-- aTag버전 -->
+      <div class="custom-window">
+        <a href="#" class="custom-a" @click="view('home')">HomeWelcom</a>
+        <a href="#" class="custom-a" @click="view('counter')">Counter</a>
+        {{ page }}
+        <HomeWelcom v-if="page === 'home'" />
+        <Counter v-else />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import Welcom from './components/Welcom.vue'
+const checking = ref(false)
+const myComponent = shallowRef()
+
+function toggle() {
+  checking.value = !checking.value
+  myComponent.value
+}
+
+watch(checking, (newChecking) => {
+  newChecking
+    ? (myComponent.value = resolveComponent('HomeWelcom'))
+    : (myComponent.value = resolveComponent('Counter'))
+})
+
+const page = ref('home')
+function view(name) {
+  page.value = name
+}
 </script>
+
+<style>
+.custom-box {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  border: 1px dotted blue;
+  gap: 2px;
+}
+
+.custom-a {
+  border: 1px solid green;
+  cursor: pointer;
+}
+
+.custom-window {
+  border: 2px solid black;
+  width: 45%;
+}
+</style>
